2f33b280f63d5a5d8892ca662f73cedca92974d4
addogra <dogra.aditya@gmail.com>
2017-08-10 17:45:57 +0530

    Updated GIT

--- mininet/configs/leaf1/l3_int_ref_topo/startup_config.sh
+++ mininet/configs/leaf1/l3_int_ref_topo/startup_config.sh
@@ -2,15 +2,15 @@
 
 stty -echo; set +m
 
-ip link set dev swp1 address 00:01:00:00:00:01
-ip link set dev swp2 address 00:01:00:00:00:02
-ip link set dev swp3 address 00:01:00:00:00:03
-ip link set dev swp4 address 00:01:00:00:00:04
+ip link set dev leaf1-eth1 address 00:01:00:00:00:01
+ip link set dev leaf1-eth2 address 00:01:00:00:00:02
+ip link set dev leaf1-eth3 address 00:01:00:00:00:03
+ip link set dev leaf1-eth4 address 00:01:00:00:00:04
 
-ip address add 10.0.1.100/24 broadcast + dev swp1
-ip address add 10.0.2.100/24 broadcast + dev swp2
-ip address add 10.1.11.1/24 broadcast + dev swp3
-ip address add 10.1.12.1/24 broadcast + dev swp4
+ip address add 10.0.1.100/24 broadcast + dev leaf1-eth1
+ip address add 10.0.2.100/24 broadcast + dev leaf1-eth2
+ip address add 10.1.11.1/24 broadcast + dev leaf1-eth3
+ip address add 10.1.12.1/24 broadcast + dev leaf1-eth4
 
 cp /configs/quagga/* /etc/quagga/
 chown quagga.quagga /etc/quagga/*
--- mininet/configs/spine1/l3_int_ref_topo/startup_config.sh
+++ mininet/configs/spine1/l3_int_ref_topo/startup_config.sh
@@ -2,11 +2,12 @@
 
 stty -echo; set +m
 
-ip link set dev swp1 address 00:03:00:00:00:01
-ip link set dev swp2 address 00:03:00:00:00:02
+ip link set dev spine1-eth1 address 00:03:00:00:00:01
+ip link set dev spine1-eth2 address 00:03:00:00:00:02
 
-ip address add 10.1.11.2/24 broadcast + dev swp1
-ip address add 10.1.21.2/24 broadcast + dev swp2
+ip address add 10.1.11.2/24 broadcast + dev spine1-eth1
+ip address add 10.1.21.2/24 broadcast + dev spine2-eth2
+ 
 
 cp /configs/quagga/* /etc/quagga/
 chown quagga.quagga /etc/quagga/*
